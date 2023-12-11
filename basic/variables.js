f87fdd5c0eeae9f037c5832a5de274171864de59
Jyoti <117471940+er-jyotii@users.noreply.github.com>
2023-12-11 17:53:24 +0000

    conatiner config done

---
+++ basic/variables.js
@@ -0,0 +1,8 @@
+const accountId = 13
+let accouEmail = "ex@gmail.com"
+var accountpassword ="12345"
+accountCity ="Jaipur"
+
+// accountId =2 not allowed
+console.log("accountId");
+console.table([accountId , accouEmail , accountpassword , accountCity])
